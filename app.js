99f39ca029797e8aa2f436ccbbc678823114a385
William Colbert <S544228@nwmissouri.edu>
2024-04-11 22:09:35 -0500

    Added update buttons and Worked on passport and security

--- app.js
+++ app.js
@@ -3,7 +3,28 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
+//New Lines of Code Start
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+passport.use(new LocalStrategy(
+  function(username, password, done) {
+    Account.findOne({ username: username })
+      .then(function (user){
+        if (err) { return done(err); }
+        if (!user) {
+          return done(null, false, { message: 'Incorrect username.' });
+        }
+        if (!user.validPassword(password)) {
+          return done(null, false, { message: 'Incorrect password.' });
+        }
+        return done(null, user);
+      })
+      .catch(function(err){
+        return done(err)
+      })
+    })
+  )
+//End of New Lines
 require('dotenv').config();
 const connectionString = process.env.MONGO_CON
 mongoose = require('mongoose');
@@ -62,6 +83,15 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+//New Lines of Code
+app.use(require('express-session')({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+//End of New Lines
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
@@ -70,6 +100,18 @@ app.use('/doctors', doctorsRouter);
 app.use('/grid', gridRouter);
 app.use('/randomitem', randomitemRouter);
 app.use('/resource', resourceRouter);
+
+//New Lines of Code
+// passport config
+// Use the existing connection
+// The Account model
+var Account = require('./models/account');
+
+passport.use(new LocalStrategy(Account.authenticate()));
+passport.serializeUser(Account.serializeUser());
+passport.deserializeUser(Account.deserializeUser());
+
+//End of New Lines
 
 
 // catch 404 and forward to error handler
