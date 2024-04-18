edcdcb1a3608e94f92a691db40b387a5e64804cd
William Colbert <S544228@nwmissouri.edu>
2024-04-17 21:09:02 -0500

    Added buttons for detail and delete for all items

--- models/doctor.js
+++ models/doctor.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose")
 const doctorSchema = mongoose.Schema({
-    years_of_experience: Number,
+    years_of_experience: {type: Number, min:1, max: 45},
     specialty: String,
     number_of_patients: Number
 })
